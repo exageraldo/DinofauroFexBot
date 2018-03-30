59513746569e2d9be2dba7857d7dcb4b4e710bc4
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-03-30 07:21:31 -0300

    Refactor the translate function

--- translate.py
+++ translate.py
@@ -4,21 +4,22 @@ CHANGE_LETTERS = {
     'ci': 'fi', 'ce': 'fe', 'CI': 'FI', 'CE': 'FE', 'Ci': 'Fi', 'Ce': 'Fe'
     }
 
-def translate(text, remove_repeated=True):
+def remove_repeated(text):
+    flag = False
+    new_text = ''
+    for i in range(len(text)):
+        if (text[i] == 'F' or text[i] == 'f') and flag == False:
+            flag = True
+        elif (text[i] == 'F' or text[i] == 'f') and flag == True:
+            continue
+        else:
+            flag = False
+        new_text += text[i]
+    return new_text
+
+def translate(text, remove=True):
     for letter in CHANGE_LETTERS:
         text = text.replace(letter, CHANGE_LETTERS[letter])
-    if remove_repeated:
-        flag = False
-        new_text = ''
-        for i in range(len(text)):
-            if (text[i] == 'F' or text[i] == 'f') and flag == False:
-                flag = True
-            elif (text[i] == 'F' or text[i] == 'f') and flag == True:
-                continue
-            else:
-                flag = False
-
-            new_text += text[i]
-
-        return new_text
+    if remove:
+        text = remove_repeated(text)
     return text
