9c167aba5142db1ce2a13e930cbb3d0b7f567c2a
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-03-30 06:51:11 -0300

    Add translate function

---
+++ translate.py
@@ -0,0 +1,23 @@
+CHANGE_LETTERS = {
+    'b': 'f', 'j': 'f', 'p': 'f', 's': 'f', 'x': 'f', 'v': 'f', 'z': 'f', 'ç': 'f',
+    'B': 'F', 'J': 'F', 'P': 'F', 'S': 'F', 'X': 'F', 'V': 'F', 'Z': 'F', 'Ç': 'F',
+    'ci': 'fi', 'ce': 'fe', 'CI': 'FI', 'CE': 'FE', 'Ci': 'Fi', 'Ce': 'Fe'
+    }
+
+text = input()
+
+for letter in CHANGE_LETTERS:
+    text = text.replace(letter, CHANGE_LETTERS[letter])
+flag = False
+new_text = ""
+for i in range(len(text)):
+    if((text[i] == 'F' or text[i] == 'f') and flag == False):
+        flag = True
+    elif((text[i] == 'F' or text[i] == 'f') and flag == True):
+        continue
+    else:
+        flag = False
+
+    new_text += text[i]
+
+print(new_text)
