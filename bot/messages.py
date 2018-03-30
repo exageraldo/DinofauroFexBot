f9530712e1d911dd854328a49daf5c9b135917e1
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-03-30 11:50:55 -0300

    Add the messages in English and Portuguese

---
+++ bot/messages.py
@@ -0,0 +1,10 @@
+MESSAGE = {
+    "BR":{
+        "start": "Alguma mensagem de teste!",
+        "help": "Ajuda!"
+    },
+    "EN":{
+        "start": "Some test message!",
+        "help": "Help!"
+    }
+}
