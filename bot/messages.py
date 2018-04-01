30bfb30b180c3cb25bb4e43ebd51da51ed9be8e7
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-01 13:40:16 -0300

    Add all the bot messages

--- bot/messages.py
+++ bot/messages.py
@@ -1,10 +1,55 @@
 MESSAGE = {
     "BR":{
-        "start": "Alguma mensagem de teste!",
-        "help": "Ajuda!"
+        "start": (
+            "Bem vindo ao Dinofauro Fex Bot!\n"
+            "Essa é a primeira versão teste do bot (v0.1.0). "
+            "Caso tenha algum feedback, mande para os desenvolvedores (/about). "
+            "Se precisar de alguma ajuda, só chamar o comando /help."
+            ),
+        "help": (
+            "Precisando de ajuda?\n"
+            "Comandos:\n"
+            "/start - Inicia o bot\n"
+            "/help - Mostra tudo que você precisa saber (Você está aqui!)\n"
+            "/about - Informações sobre o bot e os desenvolvedores\n"
+            "O bot funciona de duas maneiras:\n"
+            "Modo Inline: Basta chamar o @DinofauroFexBot na conversa em que "
+            "você se encontra, digitar a mensagem desejada, escolher se quer "
+            "com F's repetidos (+F) ou com um único F (1F ou F), e ele enviará "
+            "a mensagem 'traduzida'.\n"
+            "Modo 'echo': Ao enviar uma mensagem para o bot, que não seja um "
+            "comando (palavra que não comece com '\') ele retornará a mensagem "
+            "'traduzida'."
+            ),
+        "about": (
+            "Esse bot é mantido por @exaGeraldo e @j_monte."
+            )
     },
     "EN":{
-        "start": "Some test message!",
-        "help": "Help!"
+        "start": (
+            "Welcome to Dinofauro Fex Bot!\n"
+            "This is the first test version of the bot (v0.1.0). "
+            "If you want to send us some feedback, please send it "
+            "to the developers (/about). If you need any help, just "
+            "call the /help command."
+            ),
+        "help": (
+            "Do you need help?\n"
+            "Commands:\n"
+            "/start - Start the bot\n"
+            "/help - Show all you need to know (You're here!)\n"
+            "/about - Info about the bot and the developers\n"
+            "This bot works in two ways:\n"
+            "Inline mode: Just call the @DinofauroFexBot in the "
+            "conversation where you are, enter the desired message, "
+            "choose if you want repeated F's (+F) or single F "
+            "(1F or F), and it will send the message 'translated'.\n"
+            "Echo mode: When sending a message to the bot, other "
+            "than a command (words beginning with \), it returns "
+            "the 'translated' message."
+            ),
+        "about": (
+            "This bot is maintained by @exaGeraldo and @j_monte."
+            )
     }
 }
