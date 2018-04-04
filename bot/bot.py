278028d58782673699795232d4e09ac4a25deafb
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-03 23:46:43 -0300

    Add a button to generate another random ipsum

--- bot/bot.py
+++ bot/bot.py
@@ -48,8 +48,9 @@ def about(bot, update):
 
 @command_counter("ipsum")
 def ipsum(bot, update):
+    keyboard = ipsum_keyboard()
     update.message.reply_text(
-        translate(ipsum_generator(randint(1, 6))))
+        translate(ipsum_generator(randint(1, 6))), reply_markup=keyboard)
 
 @echo_counter
 def translation(bot, update):
@@ -71,13 +72,24 @@ def language_keyboard(message):
     return InlineKeyboardMarkup(keyboard)
 
 
+def ipsum_keyboard():
+    keyboard = [[InlineKeyboardButton('Lorem Ipfum', callback_data='ipsum')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
 def button(bot, update):
     query = update.callback_query
-    language, message = query.data.split("/")
-    bot.edit_message_text(text=MESSAGE[language][message],
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id,
-                          reply_markup=language_keyboard(message))
+    if query.data == 'ipsum':
+        bot.edit_message_text(text=translate(ipsum_generator(randint(1, 6))),
+                            chat_id=query.message.chat_id,
+                            message_id=query.message.message_id,
+                            reply_markup=ipsum_keyboard())
+    else:
+        language, message = query.data.split("/")
+        bot.edit_message_text(text=MESSAGE[language][message],
+                        chat_id=query.message.chat_id,
+                        message_id=query.message.message_id,
+                        reply_markup=language_keyboard(message))
 
 
 @inline_counter
