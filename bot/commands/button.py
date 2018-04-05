69db2617781f322ce03942c97f4a174016b2e8d6
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-05 20:48:37 -0300

    Create function for each keyboard

--- bot/commands/button.py
+++ bot/commands/button.py
@@ -5,6 +5,22 @@ from .libs.translate import translate
 from .libs.messages import MESSAGE
 from .keyboards import ipsum_keyboard, language_keyboard
 
+
+def change_language(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(text=translate(ipsum_generator(randint(1, 6))),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          reply_markup=ipsum_keyboard())
+
+
+def random_ipsum(bot, update):
+    query = update.callback_query
+    language, message = query.data.split("/")
+    bot.edit_message_text(text=MESSAGE[language][message],
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          reply_markup=language_keyboard(message))
 def button(bot, update):
     query = update.callback_query
     if query.data == 'ipsum':
