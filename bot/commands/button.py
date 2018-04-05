808b5bccc22b9c86a3bc6f43bb4e4622e81377f3
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-05 20:38:04 -0300

    Remove keyboards packages/libs

--- bot/commands/button.py
+++ bot/commands/button.py
@@ -1,21 +1,8 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
 from random import randint
 
 from .libs.ipsum_gen import ipsum_generator
 from .libs.translate import translate
 from .libs.messages import MESSAGE
-
-def language_keyboard(message):
-    keyboard = [[InlineKeyboardButton("🇧🇷", callback_data=f'BR/{message}'),
-                 InlineKeyboardButton("🇺🇸", callback_data=f'EN/{message}')]]
-    return InlineKeyboardMarkup(keyboard)
-
-
-def ipsum_keyboard():
-    keyboard = [[InlineKeyboardButton('Lorem Ipfum', callback_data='ipsum')]]
-    return InlineKeyboardMarkup(keyboard)
-
 
 def button(bot, update):
     query = update.callback_query
