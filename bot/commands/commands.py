203c26fff313eb471e0316b7dfa939adc3621cd1
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-05 20:34:40 -0300

    Remove button packages/libs

--- bot/commands/commands.py
+++ bot/commands/commands.py
@@ -53,29 +53,3 @@ def translation(bot, update):
         message = f"1F:\n{one_f}\n\n+F:\n{more_f}"
         update.message.reply_text(
             message, reply_to_message_id=update.message.message_id)
-
-
-def language_keyboard(message):
-    keyboard = [[InlineKeyboardButton("🇧🇷", callback_data=f'BR/{message}'),
-                 InlineKeyboardButton("🇺🇸", callback_data=f'EN/{message}')]]
-    return InlineKeyboardMarkup(keyboard)
-
-
-def ipsum_keyboard():
-    keyboard = [[InlineKeyboardButton('Lorem Ipfum', callback_data='ipsum')]]
-    return InlineKeyboardMarkup(keyboard)
-
-
-def button(bot, update):
-    query = update.callback_query
-    if query.data == 'ipsum':
-        bot.edit_message_text(text=translate(ipsum_generator(randint(1, 6))),
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id,
-                              reply_markup=ipsum_keyboard())
-    else:
-        language, message = query.data.split("/")
-        bot.edit_message_text(text=MESSAGE[language][message],
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id,
-                              reply_markup=language_keyboard(message))
