677b69ec2542a4fa76de78c274738d1a0d9ec73a
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-05 20:51:54 -0300

    Create dict of functions

--- bot/commands/button.py
+++ bot/commands/button.py
@@ -21,16 +21,16 @@ def random_ipsum(bot, update):
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
                           reply_markup=language_keyboard(message))
+
+
+KEYBOARD = {
+    'language': change_language,
+    'ipsum': random_ipsum
+}
+
 def button(bot, update):
     query = update.callback_query
     if query.data == 'ipsum':
-        bot.edit_message_text(text=translate(ipsum_generator(randint(1, 6))),
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id,
-                              reply_markup=ipsum_keyboard())
+        KEYBOARD['language'](bot, update)
     else:
-        language, message = query.data.split("/")
-        bot.edit_message_text(text=MESSAGE[language][message],
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id,
-                              reply_markup=language_keyboard(message))
+        KEYBOARD['ipsum'](bot, update)
